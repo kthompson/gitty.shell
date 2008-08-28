03215b5cb7257883a28f5e70d50790731e754c65
Kevin Thompson <kevin.thompson@theautomaters.com>
2008-08-28 16:22:17 -0700

    Worked on windows explorer integration

---
+++ ExtensionFileTypesAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gitty.Shell
+{
+    [global::System.AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    sealed class ExtensionFileTypesAttribute : Attribute
+    {
+        // See the attribute guidelines at 
+        //  http://go.microsoft.com/fwlink/?LinkId=85236
+        readonly string _fileTypes;
+
+        // This is a positional argument
+        public ExtensionFileTypesAttribute(string fileTypes)
+        {
+            this.FileTypesString = fileTypes;
+
+        }
+
+        public string FileTypesString { get; private set; }
+
+        public string[] FileTypes
+        {
+            get
+            {
+                return this.FileTypesString.Split(',');
+            }
+        }
+    }
+
+}
