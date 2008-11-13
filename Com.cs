251aed1a3da1436272b8a28c866bcf2a572e22df
Kevin Thompson <mrunleaded@gmail.com>
2008-11-12 23:33:57 -0800

    Updated ContextMenuHandler to use InsertMenuItem instead of the deprecated InsertMenu

--- Com.cs
+++ Com.cs
@@ -182,4 +182,71 @@ namespace Gitty.Shell
         [MarshalAs(UnmanagedType.IUnknown)]
         public object pUnkForRelease;
     }
+
+    public static class ComHelper
+    {
+        [DllImport("Kernel32.dll")]
+        private static extern int FormatMessage(
+            [MarshalAs(UnmanagedType.I4)] ref FormatMessageFlags dwFlags, ref object lpSource, ref int dwMessageId,
+            ref int dwLanguageId, IntPtr msg, ref int nSize, ref object Arguments);
+
+
+        /// <summary> 
+/// Flags for the FormatMessage kernel32 function. 
+/// </summary> 
+        [Flags()]
+        public enum FormatMessageFlags
+        {
+            /// <summary>The lpBuffer parameter is a pointer to a PVOID pointer, and that the nSize parameter specifies the minimum number of TCHARs to allocate for an output message buffer. The function allocates a buffer large enough to hold the formatted message, and places a pointer to the allocated buffer at the address specified by lpBuffer. The caller should use the LocalFree function to free the buffer when it is no longer needed.</summary> 
+            FORMAT_MESSAGE_ALLOCATE_BUFFER = 0x00000100,
+            /// <summary>Insert sequences in the message definition are to be ignored and passed through to the output buffer unchanged. This flag is useful for fetching a message for later formatting. If this flag is set, the Arguments parameter is ignored.</summary> 
+            FORMAT_MESSAGE_IGNORE_INSERTS = 0x00000200,
+            /// <summary>The lpSource parameter is a pointer to a null-terminated message definition. The message definition may contain insert sequences, just as the message text in a message table resource may. Cannot be used with FORMAT_MESSAGE_FROM_HMODULE or FORMAT_MESSAGE_FROM_SYSTEM.</summary> 
+            FORMAT_MESSAGE_FROM_STRING = 0x00000400,
+            /// <summary>The lpSource parameter is a module handle containing the message-table resource(s) to search. If this lpSource handle is NULL, the current process's application image file will be searched. Cannot be used with FORMAT_MESSAGE_FROM_STRING.</summary> 
+            FORMAT_MESSAGE_FROM_HMODULE = 0x00000800,
+            /// <summary>The function should search the system message-table resource(s) for the requested message. If this flag is specified with FORMAT_MESSAGE_FROM_HMODULE, the function searches the system message table if the message is not found in the module specified by lpSource. Cannot be used with FORMAT_MESSAGE_FROM_STRING. 
+            /// If this flag is specified, an application can pass the result of the GetLastError function to retrieve the message text for a system-defined error.</summary> 
+            FORMAT_MESSAGE_FROM_SYSTEM = 0x00001000,
+            /// <summary>The Arguments parameter is not a va_list structure, but is a pointer to an array of values that represent the arguments. 
+            /// This flag cannot be used with 64-bit argument values. If you are using 64-bit values, you must use the va_list structure.</summary> 
+            FORMAT_MESSAGE_ARGUMENT_ARRAY = 0x00002000,
+            /// <summary>The low-order byte of dwFlags can specify the maximum width of a formatted output line. Use the FORMAT_MESSAGE_MAX_WIDTH_MASK constant and bitwise Boolean operations to set and retrieve this maximum width value.</summary> 
+            FORMAT_MESSAGE_MAX_WIDTH_MASK = 0x000000FF
+        }
+        [DllImport("kernel32.dll")]
+        private extern static int GetLastError();
+
+        public static string LastError
+        {
+            get
+            {
+                int max_length = 64000;
+                IntPtr ptr = Marshal.AllocHGlobal(max_length);
+                try
+                {
+                    FormatMessageFlags flags = FormatMessageFlags.FORMAT_MESSAGE_FROM_SYSTEM;
+                    int e = GetLastError();
+                    object source = null;
+                    int lang = 0;
+                    object args = null;
+                    int c = FormatMessage(ref flags, ref source, ref e, ref lang, ptr, ref max_length, ref args);
+                    /* 
+                        'c', the return value, is always 0. 
+                        If GetLastError() is called at this point, it will return 1812, which seems strange since I'm passing in 
+                    FORMAT_MESSAGE_FROM_SYSTEM 
+                    */
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append(e);
+                    if (c != 0)
+                        sb.AppendFormat(": {0}", Marshal.PtrToStringAnsi(ptr));
+                    return sb.ToString();
+                }
+                finally
+                {
+                    Marshal.FreeHGlobal(ptr);
+                }
+            }
+        } 
+    }
 }
--- ContextMenuHandlerBase.cs
+++ ContextMenuHandlerBase.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows.Forms;
@@ -16,33 +17,17 @@ namespace Gitty.Shell
         [EditorBrowsable(EditorBrowsableState.Never)]
         private FileNameCollection m_Files;
 
+
+        public ContextMenuHandlerBase()
+        {
+        }
         // Methods
 
         [EditorBrowsable(EditorBrowsableState.Never)]
-        private static void AddItems(Menu.MenuItemCollection items, IntPtr hMenu, int index, ref int cmdId)
+        private static void AddItems(Menu.MenuItemCollection items, IntPtr hMenu, ref int cmdId)
         {
             foreach (MenuItem menu in items)
-            {
-                string menuText = menu.Text;
-                if (menu.IsParent)
-                {
-                    IntPtr popMenu = CreatePopupMenu();
-                    InsertMenu(hMenu, index, MenuFlags.MF_BYPOSITION | MenuFlags.MF_POPUP, popMenu.ToInt32(), ref menuText);
-                    AddItems(menu.MenuItems, popMenu, 0, ref cmdId);
-                }
-                else
-                {
-                    if (menuText == "-")
-                        InsertMenu(hMenu, index, MenuFlags.MF_BYPOSITION | MenuFlags.MF_SEPARATOR, cmdId, ref menuText);
-                    else
-                        InsertMenu(hMenu, index, MenuFlags.MF_BYPOSITION, cmdId, ref menuText);
-                    
-                    cmdId++;
-                }
-                menu.Text = menuText;
-                index++;
-            }
-
+                InsertMenuItem(hMenu, menu, ref cmdId);
         }
 
         [EditorBrowsable(EditorBrowsableState.Never), ComRegisterFunction]
@@ -106,6 +91,143 @@ namespace Gitty.Shell
 
         [EditorBrowsable(EditorBrowsableState.Never), DllImport("User32", EntryPoint = "InsertMenuA", CharSet = CharSet.Ansi, SetLastError = true, ExactSpelling = true)]
         private static extern long InsertMenu(IntPtr hMenu, int nPosition, MenuFlags wFlags, int wIDNewItem, [MarshalAs(UnmanagedType.VBByRefStr)] ref string lpNewItem);
+
+        private static long InsertMenuByPosition(IntPtr hMenu, int nPosition, int wIDNewItem, [MarshalAs(UnmanagedType.VBByRefStr)] ref string lpNewItem)
+        {
+            return InsertMenuByPosition(hMenu, nPosition, wIDNewItem, ref lpNewItem, 0);
+        }
+
+        private static long InsertMenuByPosition(IntPtr hMenu, int nPosition, int wIDNewItem, [MarshalAs(UnmanagedType.VBByRefStr)] ref string lpNewItem, MenuFlags wFlags)
+        {
+            return InsertMenu(hMenu, nPosition, MenuFlags.MF_BYPOSITION | wFlags, wIDNewItem, ref lpNewItem);
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool InsertMenuItem(IntPtr hMenu, uint uItem, bool fByPosition, [In] ref MENUITEMINFO lpmii);
+
+        private static  bool InsertMenuItem(IntPtr hMenu, uint position, [In] ref MENUITEMINFO lpmii)
+        {
+            return InsertMenuItem(hMenu, position, true, ref lpmii);
+        }
+
+        private static void InsertMenuItem(IntPtr hMenu, MenuItem menu, ref int cmdId)
+        {
+            var mii = new MENUITEMINFO();
+            mii.cbSize = MENUITEMINFO.sizeOf;
+            IntPtr subMenu = IntPtr.Zero;
+
+            if (menu.Checked)
+                mii.fMask |= MENUITEMINFO_MASK.MIIM_CHECKMARKS;
+
+            if (menu.IsParent)
+            {
+                mii.fMask |= MENUITEMINFO_MASK.MIIM_SUBMENU;
+                subMenu = mii.hSubMenu = CreatePopupMenu();
+            }
+            else
+            {
+                mii.fMask = MENUITEMINFO_MASK.MIIM_ID;
+                cmdId++;
+                mii.wID = cmdId;
+            }
+
+            if (menu.Text == "-")
+                mii.fType |= MENUITEMINFO_TYPE.MFT_SEPARATOR;
+            else
+            {
+                mii.fType |= MENUITEMINFO_TYPE.MFT_STRING;
+                mii.fMask |= MENUITEMINFO_MASK.MIIM_STRING;
+                mii.dwTypeData = menu.Text;
+                mii.cch = (uint)menu.Text.Length*2;
+            }
+
+            var success = InsertMenuItem(hMenu, (uint)menu.Index, ref mii);
+            if (!success)
+            {
+                Debugger.Launch();
+                string err = ComHelper.LastError;
+            }
+
+            if (subMenu != IntPtr.Zero)    
+                AddItems(menu.MenuItems, subMenu, ref cmdId);
+            
+            //return InsertMenuItem(hMenu, position, true, ref lpmii);
+        }
+
+        
+
+        [Flags]
+        private enum MENUITEMINFO_MASK : uint
+        {
+            MIIM_STATE = 0x1,
+            MIIM_ID = 0x2,
+            MIIM_SUBMENU = 0x4,
+            MIIM_CHECKMARKS = 0x8,
+            MIIM_TYPE = 0x10,
+            MIIM_DATA = 0x20,
+            MIIM_STRING = 0x40,
+            MIIM_BITMAP = 0x80,
+            MIIM_FTYPE = 0x100,
+        }
+
+        [Flags]
+        private enum MENUITEMINFO_TYPE : uint
+        {
+            MFT_STRING = 0x0,
+            MFT_BITMAP = 0x4,
+            MFT_MENUBARBREAK = 0x20,
+            MFT_MENUBREAK = 0x40,
+            MFT_OWNERDRAW = 0x100,
+            MFT_RADIOCHECK = 0x200,
+            MFT_SEPARATOR = 0x800,
+            MFT_RIGHTORDER = 0x2000,
+        }
+
+        [Flags]
+        private enum MENUITEMINFO_STATE : uint
+        {
+            MFS_GRAYED = 0x3,
+            MFS_DISABLED = 0x1,
+            MFS_CHECKED = 0x8,
+            MFS_HILITE = 0x80,
+            MFS_DEFAULT = 0x1000,
+        }
+
+        [DllImport("gdi32.dll")]
+        public static extern bool DeleteObject(IntPtr hObject);
+
+//        BOOL InsertMenuItem(
+//    HMENU hMenu,
+//    UINT uItem,
+//    BOOL fByPosition,
+//    LPCMENUITEMINFO lpmii
+//);
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct MENUITEMINFO
+        {
+            public uint cbSize;
+            public MENUITEMINFO_MASK fMask;
+            public MENUITEMINFO_TYPE fType;
+            public MENUITEMINFO_STATE fState;
+            public int wID;
+            public IntPtr /*HMENU*/ hSubMenu;
+            public IntPtr /*HBITMAP*/ hbmpChecked;
+            public IntPtr /*HBITMAP*/ hbmpUnchecked;
+            public IntPtr /*ULONG_PTR*/ dwItemData;
+            [MarshalAs(UnmanagedType.LPStr)]
+            public String dwTypeData;
+            public uint cch;
+            public IntPtr /*HBITMAP*/ hbmpItem;
+
+
+            internal static uint sizeOf
+            {
+                get { return (uint)Marshal.SizeOf(typeof(MENUITEMINFO)); }
+            }
+            
+        }    
+
         [EditorBrowsable(EditorBrowsableState.Never)]
         void IContextMenu.InvokeCommand(ref CMINVOKECOMMANDINFO lpici)
         {
@@ -129,7 +251,7 @@ namespace Gitty.Shell
         int IContextMenu.QueryContextMenu(IntPtr hMenu, int indexMenu, int idCmdFirst, int idCmdLast, QueryContextMenuFlags uFlags)
         {
             int firstID = idCmdFirst;
-            AddItems(m_CtxMenu.MenuItems, hMenu, indexMenu, ref idCmdFirst);
+            AddItems(m_CtxMenu.MenuItems, hMenu, ref idCmdFirst);
             return (idCmdFirst - firstID);
         }
 
