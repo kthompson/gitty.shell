ad0edd9a63dcaedbff7d004495da41baabc77043
Kevin Thompson <mrunleaded@gmail.com>
2008-11-26 22:06:26 -0800

    Added script to unregister the shell extensions Disabled Gitty.Shell to build by default

---
+++ unregister.cmd
@@ -0,0 +1,5 @@
+﻿@echo off
+%SystemRoot%\Microsoft.NET\Framework\v2.0.50727\regasm -u bin\Debug\Gitty.Shell.dll
+%SystemRoot%\Microsoft.NET\Framework\v2.0.50727\regasm -u bin\Release\Gitty.Shell.dll
+echo Please restart all explorer processes
+pause
